1845c4b30df997a6f2da32df71e7471e5adbf4a7
abdelmajidlahadi672-web <abdelmajidlahadi672@gmail.com>
2026-08-12 13:37:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico"],
       manifest: {
         name: "Ma Station",
         short_name: "Ma Station",
@@ -18,8 +17,8 @@ export default defineConfig({
         orientation: "portrait",
         start_url: "/",
         icons: [
-          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icône-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icône-512.png", sizes: "512x512", type: "image/png" },
         ],
       },
     }),
